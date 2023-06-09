b3691e409b949edae0686f58271691e5a64c163d
Debjoy10 <sahadebjoy10@gmail.com>
2023-06-10 01:20:19 +0530

    Updated setup for pypi

--- setup.py
+++ setup.py
@@ -3,9 +3,16 @@ try:
 except ImportError:
     from distutils.core import setup
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(name='pytorch_speech_features',
-      version='0.0.1',
+      version='0.0.3',
       description='PyTorch Speech Feature extraction',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Debjoy Saha',
       author_email='sahadebjoy10@gmail.com',
       license='MIT',
